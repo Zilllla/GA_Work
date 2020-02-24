6cb35f342d026592135e2807396658abe116a89c
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-02-24 18:20:48 +0000

    data linked from API

--- Project1/app.js
+++ Project1/app.js
@@ -1,3 +1,4 @@
+// create carousel
 let currentImg = 0;
 let finalImg = 2;
 
@@ -20,4 +21,45 @@ $('.previous-button').on('click', () => {
   }
   const $displayImg2 = $('.carousel-images').children().eq(currentImg);
   $displayImg2.css('display', 'block');
+});
+
+// link api info on click
+// onclick for first film
+$('#film1').on('click', (event) => {
+  $.ajax({url:'http://www.omdbapi.com/?apikey=53aa2cd6&t=blood+feast'
+}).then(
+  (data) => {
+        $('#title').html(data.Title);
+        $('#year').html(data.Year);
+        $('#rating').html(data.Rated);
+        $('#director').html(data.Director);
+        $('#actors').html(data.Actors);
+        $('#plot').html(data.Plot);
+  })
+})
+// onclick for second film
+$('#film2').on('click', (event) => {
+  $.ajax({url:'http://www.omdbapi.com/?apikey=53aa2cd6&t=pink+flamingos'
+}).then(
+  (data) => {
+        $('#title').html(data.Title);
+        $('#year').html(data.Year);
+        $('#rating').html(data.Rated);
+        $('#director').html(data.Director);
+        $('#actors').html(data.Actors);
+        $('#plot').html(data.Plot);
+  })
+})
+// onclick for third film
+$('#film3').on('click', (event) => {
+  $.ajax({url:'http://www.omdbapi.com/?apikey=53aa2cd6&t=santa+sangre'
+}).then(
+  (data) => {
+        $('#title').html(data.Title);
+        $('#year').html(data.Year);
+        $('#rating').html(data.Rated);
+        $('#director').html(data.Director);
+        $('#actors').html(data.Actors);
+        $('#plot').html(data.Plot);
+  })
 })
