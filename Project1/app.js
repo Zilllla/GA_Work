e5cec0812864926a758a99c1d6392c75b4f3b776
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-02-24 17:10:14 +0000

    next and previous buttons working, but problem with only one img displaying initially

--- Project1/app.js
+++ Project1/app.js
@@ -1,1 +1,23 @@
-testing changes
+let currentImg = 0;
+let finalImg = 3;
+
+$('.next-button').on('click', () => {
+  const $displayImg = $('.carousel-images').children().eq(currentImg).css('display', 'none');
+    currentImg++;
+  if(currentImg > finalImg) {
+    currentImg = 0;
+  }
+  const $displayImg2 = $('.carousel-images').children().eq(currentImg);
+  $displayImg2.css('display', 'block');
+});
+
+$('.previous-button').on('click', () => {
+  const $displayImg = $('.carousel-images').children().eq(currentImg);
+  $displayImg.css('display', 'none');
+    currentImg--;
+  if(currentImg < 0) {
+    currentImg = finalImg;
+  }
+  const $displayImg2 = $('.carousel-images').children().eq(currentImg);
+  $displayImg2.css('display', 'block');
+})
